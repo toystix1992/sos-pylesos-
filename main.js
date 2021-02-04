fcc9ad7bed4e8d969c696350216f617578491e46
Parfionov Anton <toystix1992@gmail.com>
2021-02-04 18:31:26 +0300

    feat: add some featches

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ const arrowLeft = document.querySelector(".gallary__icon-l");
 const sliderItem = document.querySelectorAll(".gallary__container-item");
 const slImg = ["sl1.jpg","sl2.jpg","sl3.jpg","sl4.jpg","sl5.jpg","sl6.jpg","sl7.jpg","sl8.jpg","sl9.jpg"];
 const wayToSlImg = "./assets/img/sliderImg/";
+const headLogo = document.querySelector(".header__logo-img");
 let i = 0;
 
-
-    sliderItem[i].innerHTML = `<img class= "gallary-img" src="${wayToSlImg}${slImg[i]}" alt="1">`;
-    sliderItem[i+1].innerHTML = `<img class= "gallary-img" src="${wayToSlImg}${slImg[i+1]}" alt="1">`;
-    sliderItem[i+2].innerHTML = `<img class= "gallary-img" src="${wayToSlImg}${slImg[i+2]}" alt="1">`;
+//Make Slider
+sliderItem[i+1].innerHTML = `<img class= "gallary-img" src="${wayToSlImg}${slImg[i+1]}" alt="1">`;
+sliderItem[i+2].innerHTML = `<img class= "gallary-img" src="${wayToSlImg}${slImg[i+2]}" alt="1">`;
+sliderItem[i].innerHTML = `<img class= "gallary-img" src="${wayToSlImg}${slImg[i]}" alt="1">`;
 
 
 let scrRight = () => {
@@ -33,9 +34,19 @@ let scrLeft = () => {
     sliderItem[2].innerHTML = `<img class= "gallary-img" src="${wayToSlImg}${slImg[i+2]}" alt="1">`;
 }
 
-
 arrowRight.addEventListener("click", scrRight);
 arrowLeft.addEventListener("click", scrLeft);
+
+//Button UP
+console.log(document.body.scrollHeight);
+
+const HeadAnimLogo = () => {
+    }
+
+HeadAnimLogo();
+
+
+
 
 
 
